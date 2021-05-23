ef21f8cf60a0c0adabd7a69bf15543142c39f89d
Hampus Engström <hampus.engstrom@cybercom.com>
2021-05-23 21:30:04 +0200

    test: add test for handling fetch success/failure

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('fetch mocked data failure', async () => {
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockRejectedValue('API is down'),
+  });
+  const { getByTestId } = render(<App />);
+  const errorNode = await waitFor(() => getByTestId('error'));
+  expect(errorNode).toBeInTheDocument();
+  jest.restoreAllMocks();
+});
+
+test('fetch mocked data failure', async () => {
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockResolvedValue([]),
+  });
+  const { getByTestId } = render(<App />);
+  const successNode = await waitFor(() => getByTestId('success'));
+  expect(successNode).toBeInTheDocument();
+  jest.restoreAllMocks();
 });
